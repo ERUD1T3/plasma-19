83c7dab150f3617d8ce8bb6f16e31c1bc0d55e69
PascalPolygon <pdao2015@my.fit.edu>
2020-04-05 01:22:14 -0400

    Boilerplate map

---
+++ public/javaScript/map.js
@@ -0,0 +1,340 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicGRhbzIwMTUiLCJhIjoiY2p5ZDlqNnI4MG84NDNjcW14bzAwbXVpbSJ9.-GnGL_1vL8oZm_XHI9jSqw";
+
+var donors = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.034084142948, 38.909671288923],
+      },
+      properties: {
+        phoneFormatted: "(202) 234-7336",
+        phone: "2022347336",
+        address: "1471 P St NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at 15th St NW",
+        postalCode: "20005",
+        state: "D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.049766, 38.900772],
+      },
+      properties: {
+        phoneFormatted: "(202) 507-8357",
+        phone: "2025078357",
+        address: "2221 I St NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at 22nd St NW",
+        postalCode: "20037",
+        state: "D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.043929, 38.910525],
+      },
+      properties: {
+        phoneFormatted: "(202) 387-9338",
+        phone: "2023879338",
+        address: "1512 Connecticut Ave NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at Dupont Circle",
+        postalCode: "20036",
+        state: "D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.0672, 38.90516896],
+      },
+      properties: {
+        phoneFormatted: "(202) 337-9338",
+        phone: "2023379338",
+        address: "3333 M St NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at 34th St NW",
+        postalCode: "20007",
+        state: "D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.002583742142, 38.887041080933],
+      },
+      properties: {
+        phoneFormatted: "(202) 547-9338",
+        phone: "2025479338",
+        address: "221 Pennsylvania Ave SE",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "btwn 2nd & 3rd Sts. SE",
+        postalCode: "20003",
+        state: "D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-76.933492720127, 38.99225245786],
+      },
+      properties: {
+        address: "8204 Baltimore Ave",
+        city: "College Park",
+        country: "United States",
+        postalCode: "20740",
+        state: "MD",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.097083330154, 38.980979],
+      },
+      properties: {
+        phoneFormatted: "(301) 654-7336",
+        phone: "3016547336",
+        address: "4831 Bethesda Ave",
+        cc: "US",
+        city: "Bethesda",
+        country: "United States",
+        postalCode: "20814",
+        state: "MD",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.359425054188, 38.958058116661],
+      },
+      properties: {
+        phoneFormatted: "(571) 203-0082",
+        phone: "5712030082",
+        address: "11935 Democracy Dr",
+        city: "Reston",
+        country: "United States",
+        crossStreet: "btw Explorer & Library",
+        postalCode: "20190",
+        state: "VA",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.10853099823, 38.880100922392],
+      },
+      properties: {
+        phoneFormatted: "(703) 522-2016",
+        phone: "7035222016",
+        address: "4075 Wilson Blvd",
+        city: "Arlington",
+        country: "United States",
+        crossStreet: "at N Randolph St.",
+        postalCode: "22203",
+        state: "VA",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-75.28784, 40.008008],
+      },
+      properties: {
+        phoneFormatted: "(610) 642-9400",
+        phone: "6106429400",
+        address: "68 Coulter Ave",
+        city: "Ardmore",
+        country: "United States",
+        postalCode: "19003",
+        state: "PA",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-75.20121216774, 39.954030175164],
+      },
+      properties: {
+        phoneFormatted: "(215) 386-1365",
+        phone: "2153861365",
+        address: "3925 Walnut St",
+        city: "Philadelphia",
+        country: "United States",
+        postalCode: "19104",
+        state: "PA",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.043959498405, 38.903883387232],
+      },
+      properties: {
+        phoneFormatted: "(202) 331-3355",
+        phone: "2023313355",
+        address: "1901 L St. NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at 19th St",
+        postalCode: "20036",
+        state: "D.C.",
+      },
+    },
+  ],
+};
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v10",
+  center: [-77.034084, 38.909671],
+  zoom: 14,
+});
+
+donors.features.forEach(function (donor, i) {
+  donor.properties.id = i;
+});
+
+map.on("load", function (e) {
+  /* Add the data to your map as a layer */
+  map.addLayer({
+    id: "locations",
+    type: "symbol",
+    /* Add a GeoJSON source containing place coordinates and information. */
+    source: {
+      type: "geojson",
+      data: donors,
+    },
+    layout: {
+      //"icon-image": "restaurant-15",
+      "icon-allow-overlap": true,
+    },
+  });
+
+  map.addSource("places", {
+    type: "geojson",
+    data: donors,
+  });
+
+  addMarkers();
+
+  buildLocationList(donors);
+});
+
+function buildLocationList(data) {
+  data.features.forEach(function (donor, i) {
+    /**
+     * Create a shortcut for `donor.properties`,
+     * which will be used several times below.
+     **/
+    var prop = donor.properties;
+
+    /* Add a new listing section to the sidebar. */
+    var listings = document.getElementById("listings");
+    var listing = listings.appendChild(document.createElement("div"));
+    /* Assign a unique `id` to the listing. */
+    listing.id = "listing-" + prop.id;
+    /* Assign the `item` class to each listing for styling. */
+    listing.className = "item";
+
+    /* Add the link to the individual listing created above. */
+    var link = listing.appendChild(document.createElement("a"));
+    link.href = "#";
+    link.className = "title";
+    link.id = "link-" + prop.id;
+    link.innerHTML = prop.address;
+
+    /* Add details to the individual listing. */
+    var details = listing.appendChild(document.createElement("div"));
+    details.innerHTML = prop.city;
+    if (prop.phone) {
+      details.innerHTML += " · " + prop.phoneFormatted;
+    }
+  });
+}
+
+function addMarkers() {
+  /* For each feature in the GeoJSON object above: */
+  donors.features.forEach(function (marker) {
+    /* Create a div element for the marker. */
+    var el = document.createElement("div");
+    /* Assign a unique `id` to the marker. */
+    el.id = "marker-" + marker.properties.id;
+    /* Assign the `marker` class to each marker for styling. */
+    el.className = "marker";
+
+    /**
+     * Create a marker using the div element
+     * defined above and add it to the map.
+     **/
+    new mapboxgl.Marker(el, { offset: [0, -23] })
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+
+    //Flying to location and popups
+    el.addEventListener("click", function (e) {
+      /* Fly to the point */
+      flyToStore(marker);
+      /* Close all other popups and display popup for clicked store */
+      createPopUp(marker);
+      /* Highlight listing in sidebar */
+      var activeItem = document.getElementsByClassName("active");
+      e.stopPropagation();
+      if (activeItem[0]) {
+        activeItem[0].classList.remove("active");
+      }
+      var listing = document.getElementById("listing-" + marker.properties.id);
+      listing.classList.add("active");
+    });
+  });
+}
+
+/**
+ * Use Mapbox GL JS's `flyTo` to move the camera smoothly
+ * a given center point.
+ **/
+function flyToStore(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 15,
+  });
+}
+
+/**
+ * Create a Mapbox GL JS `Popup`.
+ **/
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName("mapboxgl-popup");
+  if (popUps[0]) popUps[0].remove();
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML(
+      "<h3>Sweetgreen</h3>" +
+        "<h4>" +
+        currentFeature.properties.address +
+        "</h4>"
+    )
+    .addTo(map);
+}
