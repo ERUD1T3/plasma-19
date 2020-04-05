4fc04eb7c43f4ffc982b736437ddab897a3989da
PascalPolygon <pdao2015@my.fit.edu>
2020-04-05 00:17:47 -0400

    ejs and navbar

--- routes/donor.route.js
+++ routes/donor.route.js
@@ -1,13 +1,10 @@
 // route for user request
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    console.log('Root page')
-    res.send("hello world")
-    // res.render('main')
-})
-
-
-
+router.get("/", (req, res) => {
+  console.log("Root page");
+  //res.send("hello world")
+  res.render("main");
+});
 
 module.exports = router;
