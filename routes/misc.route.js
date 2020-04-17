483092c4aea2a17c99944d47c3121946718abf5b
ERUD1T3 <josiasmoukpe@gmail.com>
2020-04-17 15:25:02 -0400

    added verified field and ssadmin page

--- routes/misc.route.js
+++ routes/misc.route.js
@@ -1,4 +1,6 @@
 // route for miscellaneous request
+const Donor = require('../model/donor')
+const fileStreamDriver = require("../drivers/fileStreamDriver");
 const router = require('express').Router()
 
 
@@ -14,6 +16,45 @@ router.get("/team", (req, res) => {
     res.render("contactus");
 });
 
+router.get("/ssadmin", (req, res) => {
+    console.log("found admin page")
+    var errors = [];
+    // getting list of donors 
+    Donor.aggregate([
+        {
+            $match: {
+                verified: false,
+            },
+        }
+    ], (error, unv_donors) => {
+        if(error) {
+            console.log(error);
+            errors.push({
+                msg: 'Error finding unverified donors'
+            })
+            res.render('admin', {
+                errors
+            })
+        } else
+        if(!unv_donors) {
+            console.log('No unverified donors found')
+            req.flash(
+                "success_msg",
+                "All Donors have been verified. No unverified donors!"
+            )
+            res.redirect('/')
+        }
+        else {
+            console.log('rendiring list of unverified donors on front end')
+            res.render('admin', {
+                donors: unv_donors,
+            })
+        }
+        
+    })
+
+    
+})
 
 
 
