c073a7d2650a2fc48ac7e37ccd317139f93fd8f6
ERUD1T3 <josiasmoukpe@gmail.com>
2020-04-19 17:02:24 -0400

    quick fix

--- public/javaScript/scripts.js
+++ public/javaScript/scripts.js
@@ -1,5 +1,5 @@
 function disclaimer() {
-    confirm(
+    alert(
         `You are about to be redirected to an Official Website.\
         We are in no terms attached, or affiliated to that Website.\nWe simply redirect in an effort to provide free information.`
         );
