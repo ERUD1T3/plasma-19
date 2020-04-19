9e7385b82588a2fc8939a26ae8af7bbdf4947f39
ERUD1T3 <josiasmoukpe@gmail.com>
2020-04-19 16:59:51 -0400

    added disclaimer for link redirect

---
+++ public/javaScript/scripts.js
@@ -0,0 +1,6 @@
+function disclaimer() {
+    confirm(
+        `You are about to be redirected to an Official Website.\
+        We are in no terms attached, or affiliated to that Website.\nWe simply redirect in an effort to provide free information.`
+        );
+}
